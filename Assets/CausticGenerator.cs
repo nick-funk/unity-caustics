642d47ae7b25a511e10b9c55d3033843249245b3
nick-funk <code@nickfunk.com>
2023-06-07 22:59:06 -0600

    blur output image to lessen sample artifacts

--- Assets/CausticGenerator.cs
+++ Assets/CausticGenerator.cs
@@ -226,12 +226,64 @@ public class CausticGenerator : EditorWindow
 
         texture.Apply();
 
-        byte[] bytes = ImageConversion.EncodeToPNG(texture);
-        DestroyImmediate(texture);
+        var blurred = blurTexture(texture, 8);
+        byte[] bytes = ImageConversion.EncodeToPNG(blurred);
 
-        var path = Application.dataPath + "/../caustics/caustic.png";
+        DestroyImmediate(texture);
+        DestroyImmediate(blurred);
+
+        var path = Application.dataPath + "/../gen-output/caustic.png";
         Directory.CreateDirectory(Path.GetDirectoryName(path));
         File.WriteAllBytes(path, bytes);
+    }
+
+    private Texture2D blurTexture(Texture2D image, int blurSize)
+    {
+        Texture2D blurred = new Texture2D(image.width, image.height);
+
+        // look at every pixel in the blur rectangle
+        for (int xx = 0; xx < image.width; xx++)
+        {
+            for (int yy = 0; yy < image.height; yy++)
+            {
+                float avgR = 0, avgG = 0, avgB = 0, avgA = 0;
+                int blurPixelCount = 0;
+
+                // average the color of the red, green and blue for each pixel in the
+                // blur size while making sure you don't go outside the image bounds
+                for (int x = xx; (x < xx + blurSize && x < image.width) ; x++)
+                {
+                    for (int y = yy; (y < yy + blurSize && y < image.height) ; y++)
+                    {
+                        Color pixel = image.GetPixel(x, y);
+
+                        avgR += pixel.r;
+                        avgG += pixel.g;
+                        avgB += pixel.b;
+                        avgA += pixel.a;
+
+                        blurPixelCount++;
+                    }
+                }
+
+                avgR = avgR / blurPixelCount;
+                avgG = avgG / blurPixelCount;
+                avgB = avgB / blurPixelCount;
+                avgA = avgA / blurPixelCount;
+
+                // now that we know the average for the blur size, set each pixel to that color
+                for (int x = xx; x < xx + blurSize &&  x < image.width; x++)
+                {
+                    for (int y = yy; y < yy + blurSize && y < image.height; y++)
+                    {
+                        blurred.SetPixel(x, y, new Color(avgR, avgG, avgB, avgA));
+                    }
+                }
+            }
+        }
+
+        blurred.Apply();
+        return blurred;
     }
 
     private List<Vector3> castLight(
